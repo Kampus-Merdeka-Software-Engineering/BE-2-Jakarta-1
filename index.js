53db8855fc8a10b9d3566d0cc9202589ed113730
qurunul <qurunulbahri1@gmail.com>
2023-12-09 00:41:18 +0700

    update telat

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use((req, res) => {
-    res.status(404).json({
-        message: 'Route not found'
-    })
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 // Login Routes
 app.use('/login', loginRouter);
-
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
