7531d3b1366c7c002adec97118e52a0c981054fc
qurunul <qurunulbahri1@gmail.com>
2023-12-12 11:54:02 +0700

    fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const bcrypt = require('bcrypt');
-const userRouter = require('./routes/userRoute');
-const loginRouter = require('./routes/loginRoute');
+const userRouter = require('routes/userRoute.js');
+const loginRouter = require('routes/loginRoute.js');
 const app = express();
 const port = 3000;
 
